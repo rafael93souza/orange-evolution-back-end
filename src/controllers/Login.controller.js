cbc495aed2cb4b8e50b3231b3691c646c3d62c87
Emerson Moreira <eemr3@yahoo.com.br>
2022-11-03 16:38:26 -0300

    feat: :fire: Login.controller.js
    cria o controlador de login

---
+++ src/controllers/Login.controller.js
@@ -0,0 +1,13 @@
+const LoginService = require('../services/Login.service');
+
+const signIn = async (req, res) => {
+  try {
+    const token = await LoginService.signIn(req.body);
+    return res.status(200).json(token);
+  } catch (error) {
+    console.log(error);
+    return res.status(error.status).json({ message: error.message });
+  }
+};
+
+module.exports = { signIn };
