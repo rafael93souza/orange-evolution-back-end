290839c8d2f986758bfd6345df995d9a07a9d367
Emerson Moreira <eemr3@yahoo.com.br>
2022-11-03 15:21:02 -0300

    refactor: :zap: index.js
    adiciona informação da porta ao subir o server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-require("dotenv").config({})
-const express = require("express")
+require('dotenv').config({});
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const router = require("./routers/router");
+const cors = require('cors');
+const router = require('./routers/router');
 
 app.use(cors());
 app.use(express.json());
-app.use(router)
+app.use(router);
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () =>
+  console.info(`App rodando na porta ${process.env.PORT}`),
+);
