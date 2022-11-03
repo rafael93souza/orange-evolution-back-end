d5642aa63b6990bdcdae31d6ef391c879c1b2df3
Emerson Moreira <eemr3@yahoo.com.br>
2022-11-03 16:38:43 -0300

    feat: :fire: router.js
    adiciona a rota para login

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const UseController = require('../controllers/User.controllers');
+const LoginController = require('../controllers/Login.controller');
 const { middlewaresTest } = require('../middlewares/middlewaresTest');
 const router = express.Router();
 
 router.post('/users', middlewaresTest, UseController.create);
 router.get('/users', middlewaresTest, UseController.findAll);
+router.post('/login', LoginController.signIn);
 
 module.exports = router;
