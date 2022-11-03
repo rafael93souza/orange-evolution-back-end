ea169d72e720d1d6e16a13ff841ab35d23445808
Emerson Moreira <eemr3@yahoo.com.br>
2022-11-03 10:15:55 -0300

    feat(knex): configura conexao com o banco de dados

--- src/connections/database.js
+++ src/connections/database.js
@@ -1,1 +1,13 @@
-// Conexão com banco de dados 
+// Conexão com banco de dados
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+  },
+});
+
+module.exports = knex;
