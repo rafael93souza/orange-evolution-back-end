f44651a2aaea54e3bcae2ec962dcd9f5d47489ef
Emerson Moreira <eemr3@yahoo.com.br>
2022-11-03 15:19:44 -0300

    feat: :fire: User.controller
    cria função create e findAll

---
+++ src/controllers/User.controllers.js
@@ -0,0 +1,17 @@
+const UserService = require('../services/User.service');
+
+const create = async (req, res) => {
+  try {
+    const user = await UserService.create(req.body);
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(409).json({ message: error.message });
+  }
+};
+
+const findAll = async (req, res) => {
+  const users = await UserService.findAll();
+  return res.status(200).json(users);
+};
+
+module.exports = { create, findAll };
