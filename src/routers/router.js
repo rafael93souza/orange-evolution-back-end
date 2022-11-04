ecb0671bfd85424fe85e3f73439b7baea6745d12
Rafael Souza <rafael93.souzasfc5@gmail.com>
2022-11-04 12:23:38 -0300

    Adicionado os schemas de validação dos dados, nas rotas de login, e de cadastro do usuario

--- src/routers/router.js
+++ src/routers/router.js
@@ -2,11 +2,13 @@ const express = require('express');
 const UseController = require('../controllers/User.controllers');
 const LoginController = require('../controllers/Login.controller');
 const { authMiddleware } = require('../middlewares/auth.middleware');
+const validationSchema = require('../middlewares/validationSchema');
+const schemaRegisterUser = require('../schemas/schemaRegisterUser');
+const schemaLoginUser = require('../schemas/schemaLoginUser');
 const router = express.Router();
 
-router.post('/users', UseController.create);
+router.post('/users', validationSchema(schemaRegisterUser), UseController.create);
 router.get('/users', authMiddleware, UseController.findAll);
-
-router.post('/login', LoginController.signIn);
+router.post('/login', validationSchema(schemaLoginUser), LoginController.signIn);
 
 module.exports = router;
