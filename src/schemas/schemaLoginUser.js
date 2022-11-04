940ae72ded8a8aee20edcacf5ee54d6312d193d2
Emerson Moreira <eemr3@yahoo.com.br>
2022-11-04 19:41:50 -0300

    refactor: :zap: modifica propriedades name, password para nome e senha respectivamente

--- src/schemas/schemaLoginUser.js
+++ src/schemas/schemaLoginUser.js
@@ -1,12 +1,12 @@
-const joi = require("joi")
+const joi = require('joi');
 const schemaLoginUser = joi.object({
-    email: joi.string().email().required().trim().messages({
-        'any.required': "O campo email é obrigatótio",
-        'string.empty': "O campo email é obrigatótio"
-    }),
-    password: joi.string().required().trim().messages({
-        'any.required': "O campo senha é obrigatótio",
-        'string.empty': "O campo senha é obrigatótio"
-    })
+  email: joi.string().email().required().trim().messages({
+    'any.required': 'O campo email é obrigatótio',
+    'string.empty': 'O campo email é obrigatótio',
+  }),
+  senha: joi.string().required().trim().messages({
+    'any.required': 'O campo senha é obrigatótio',
+    'string.empty': 'O campo senha é obrigatótio',
+  }),
 });
 module.exports = schemaLoginUser;
