5f70ab545ce788cbc53d299091ccb891ebbb9ece
Rafael Souza <rafael93.souzasfc5@gmail.com>
2022-11-04 22:00:59 -0300

    Criado services de cadastrar tilhas e listar trilhas

---
+++ src/services/Trails.service.js
@@ -0,0 +1,26 @@
+const knex = require('../connections/database');
+const errors = require('../utils/errorsBase');
+
+const create = async (data) => {
+    const trailExists = await knex('cursos').where('nome', data.nome).first();
+
+    if (trailExists) {
+        throw errors(409, 'Trilha já cadastrada no sistema!');
+    }
+
+    const createdTrail = await knex('cursos').insert(data).returning('*');
+
+    return { success: createdTrail };
+};
+
+const findAll = async () => {
+    const trails = await knex('cursos');
+    return trails;
+};
+
+module.exports = {
+    create,
+    findAll,
+    update,
+    deleteTrail
+};
