878473ec32569db89adcdb4aa7dedc216fedd5a9
eemr3 <eemr3@yahoo.com.br>
2022-11-12 18:42:03 -0300

    docs: :memo: Swagger Document
    cria documentação da API utilizando o Swagger como ferramenta

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 require('express-async-errors');
 require('dotenv').config({});
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swaggerDocument.json');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const router = require('./routers/router');
 const { errorMiddleware } = require('./middlewares/error.middleware');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors());
 app.use(express.json());
