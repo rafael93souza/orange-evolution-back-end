e0df448e7840fb199a9508f07a97b4ce29dd2f01
Rafael Souza <rafael93.souzasfc5@gmail.com>
2022-11-15 11:29:41 -0300

    Removendo logs

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -2,7 +2,6 @@ const errorMiddleware = (error, _req, res, _next) => {
     if (error.status) {
         return res.status(error.status).json({ message: error.message });
     }
-    console.log(error.message)
     return res.status(500).json({ message: 'Internal server Error!' });
 };
 
