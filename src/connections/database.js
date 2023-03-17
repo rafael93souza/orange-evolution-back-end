3d3d8939c93cac0b33c73a2e297c2fa19ad47fe7
Rafael Souza <rafael93.souzasfc5@gmail.com>
2023-03-17 16:33:45 -0300

    feat: Modificado Conexão com Banco de dados da aplicação

--- src/connections/database.js
+++ src/connections/database.js
@@ -1,14 +1,8 @@
 // Conexão com banco de dados
 const knex = require('knex')({
     client: 'pg',
-    connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PWD,
-        database: process.env.DB_NAME,
-        ssl: { rejectUnauthorized: false }
-    },
+    connection: process.env.PG_CONNECTION_STRING,
+    searchPath: ['knex', 'public'],
 });
 
 module.exports = knex;
--- src/controllers/Admin.controllers.js
+++ src/controllers/Admin.controllers.js
@@ -1,13 +1,13 @@
 const AdminService = require('../services/Admin.service');
 
 const create = async (req, res) => {
-  const administrator = await AdminService.create(req.body);
-  return res.status(201).json(administrator);
+    const administrator = await AdminService.create(req.body);
+    return res.status(201).json(administrator);
 };
 
 const findAll = async (req_, res) => {
-  const administrators = await AdminService.findAll();
-  return res.status(200).json(administrators);
+    const administrators = await AdminService.findAll();
+    return res.status(200).json(administrators);
 };
 
 module.exports = { create, findAll };
